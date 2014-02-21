b358deb137b4817d4c5ffe882a78dd846d283633
fabrilov <fabrilov@gmail.com>
2014-02-21 09:40:40 +0100

    eliminata dal JS la scritta Dabpumps
    eliminata dal JS la scritta Dabpumps perchè già cionfigurata nell'HTML

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'puBRA5t636E6WhJXi9xfa3xWP5Dzu4kA6x8vUh6UqNDCwDnt', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1509467466], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'DABPUMPS', // Replaces Xively logo in the header
+		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '024314', // CSS HEX value of color to represent data (omit leading #)
